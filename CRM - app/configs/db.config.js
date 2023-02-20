3a4be584fbc941d2d0a99d1783efc98004a0c47e
Aradhya Gupta <aradhyagupta445@gmail.com>
2023-02-21 02:55:08 +0530

    Change DB

--- CRM - app/configs/db.config.js
+++ CRM - app/configs/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     DB_NAME: "crm_db",
-    DB_URL: "mongodb://localhost:27017/crm_db"
+    DB_URL: "mongodb+srv://Aradhya:Hapl8@crm.2kriyoy.mongodb.net/?retryWrites=true&w=majority"
 }
--- cron-job/configs/db.config.js
+++ cron-job/configs/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     DB_NAME: "notification_db",
-    DB_URL: "mongodb://localhost:27017/notification_db"
+    DB_URL: "mongodb+srv://Aradhya:Hapl8@crm.2kriyoy.mongodb.net/?retryWrites=true&w=majority"
 }
